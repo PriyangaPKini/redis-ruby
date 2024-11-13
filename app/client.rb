a70fb5ac7eed76622463547b123cfa2b315e7720
Priyanga P Kini <priyangapkini99@gmail.com>
2024-11-13 15:41:26 +0530

    Move client handling code to server class

--- app/client.rb
+++ app/client.rb
@@ -1,52 +1,31 @@
 require "socket"
 
 # frozen_string_literal: true
+module Redis
+  class RedisClient
 
-class RedisClient
+    attr_reader :port
 
-  attr_reader :client
-
-  def initialize(port, *args)
-    @client = RedisServer.new(port).start
-  end
-
-  def parse_commands
-    command = []
-    first_line = client.gets&.strip
-    if first_line && first_line.start_with?('*')
-      num_elements = first_line[1..].to_i
-
-      num_elements.times do
-        length_line = client.gets&.strip
-        if length_line && length_line.start_with?('$')
-          str_length = length_line[1..].to_i
-          element = client.read(str_length)&.strip
-          client.gets
-          command << element.upcase
-        else
-          client.puts "-ERR protocol error"
-          return nil
-        end
-      end
-      return command
-    else
-      client.puts "-ERR unknown command format"
-      return nil
+    def initialize(port, *args)
+      @port = port
     end
-  end
 
-  def execute
-    loop do
-      queries = parse_commands
-      case queries
-      when %w[COMMAND DOCS]
-        client.puts "+OK\r\n"
-      when ["PING"]
-        client.puts("+PONG\r\n")
-      else
-        puts("Error in your query  #{ queries }")
-        client.puts("-ERR unknown command #{ queries }\r\n")
+    def execute
+      client = TCPSocket.new("127.0.0.1", port)
+      puts "Connected to server on port #{port}"
+      client.puts("*1\r\n$4\r\nPING\r\n")
+      while line = client.gets do
+        puts line
+        break if line.length
+        client.puts(line)
+        puts "Server response: #{line}"
       end
+      client.close
+    rescue => e
+      puts "Connection encountered an error: #{e.message}"
     end
   end
 end
+
+client = Redis::RedisClient.new(6380)
+client.execute
