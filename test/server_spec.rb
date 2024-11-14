fffb925f337d7bbda7fef378c99b9c0a74b4ecf1
Priyanga P Kini <priyangapkini99@gmail.com>
2024-11-14 21:23:16 +0530

    write unit tests for parser and command executer

--- test/server_spec.rb
+++ test/server_spec.rb
@@ -1,81 +1,103 @@
 require 'socket'
+require 'rspec'
+
 
 require_relative '../app/server'
 
 RSpec.describe Redis::Server do
-  let(:port) { 6381 }
-  let(:server) { Redis::Server.new(port) }
-
-  before(:all) do
-    @server_thread = Thread.new { Redis::Server.new(6381).start }
-    sleep 0.5
-  end
-
-  after(:all) do
-    Thread.kill(@server_thread)
-  end
-
-  def send_command(command)
-    client = TCPSocket.new("127.0.0.1", port)
-    client.puts command
-    response = client.readpartial(1024)
-    client.close
-    response
-  end
+  let(:port) { 6379 }
+  let(:server) { described_class.new(port) }
+  let(:client) { instance_double("TCPSocket") }
 
   describe "#parse_query" do
+    it "returns an error when line starts with anything other than '*'" do
+      allow(client).to receive(:gets).and_return("#7\r\n","UNKNOWN\r\n")
+      expect(client).to receive(:puts).with("-ERR unknown command format\r\n")
 
-    it "returns an error for protocol error" do
-      response = send_command("*1\r\n*1\r\nECHO\r\n")
-      expect(response).to eq("-ERR protocol error\r\n")
+      response = server.parse_query(client)
+      expect(response).to be_nil
     end
 
-    it "returns an error for unknown command format" do
-      response = send_command("#7\r\nUNKNOWN\r\n")
-      expect(response).to eq("-ERR unknown command format\r\n")
+    it "returns an error when length indicator does not start with '$'" do
+      allow(client).to receive(:gets).and_return("*1\r\n","*1\r\n","PING\r\n")
+      expect(client).to receive(:puts).with("-ERR protocol error\r\n")
+
+      response = server.parse_query(client)
+      expect(response).to be_nil
     end
+
+    it "returns an error when specified length is zero" do
+      allow(client).to receive(:gets).and_return("*1\r\n","$0\r\n","PING\r\n")
+      expect(client).to receive(:puts).with("-ERR protocol error\r\n")
+
+      response = server.parse_query(client)
+      expect(response).to be_nil
+    end
+
+    it "returns an error when specified length mismatches the length of any of the array element" do
+      allow(client).to receive(:gets).and_return("*1\r\n","$5\r\n","PING\r\n")
+      expect(client).to receive(:puts).with("-ERR unknown command format\r\n")
+
+      response = server.parse_query(client)
+      expect(response).to be_nil
+
+      allow(client).to receive(:gets).and_return("*1\r\n","$3\r\n","y\r\n")
+      expect(client).to receive(:puts).with("-ERR unknown command format\r\n")
+
+      response = server.parse_query(client)
+      expect(response).to be_nil
+    end
+
+    it "returns an array of commands and arguments on successful parsing" do
+      allow(client).to receive(:gets).and_return("*2\r\n", "$3\r\n", "GET\r\n", "$4\r\n", "test\r\n")
+      result = server.parse_query(client)
+      expect(result).to eq(%w[GET test])
+    end
+
   end
 
   describe "#execute" do
     it "returns an error for unknown command" do
-      response = send_command("*1\r\n$7\r\nUNKNOWN\r\n")
-      expect(response).to eq("-ERR unknown command\r\n")
+      expect(client).to receive(:puts).with("-ERR unknown command\r\n")
+      response = server.execute(client, %w[UNKNOWN])
+      expect(response).to be_nil
     end
 
     it "responds with +PONG for PING command" do
-      response = send_command("*1\r\n$4\r\nPING\r\n")
-      expect(response).to eq("+PONG\r\n")
+      expect(client).to receive(:puts).with("+PONG\r\n")
+      server.execute(client, %w[PING])
     end
 
     it "responds with +OK for COMMAND DOCS" do
-      response = send_command("*2\r\n$7\r\nCOMMAND\r\n$4\r\nDOCS\r\n")
-      expect(response).to eq("+OK\r\n")
+      expect(client).to receive(:puts).with("+OK\r\n")
+      server.execute(client, %w[COMMAND DOCS])
     end
 
     it "echoes back the argument for ECHO" do
-      response = send_command("*2\r\n$4\r\nECHO\r\n$5\r\nHello\r\n")
-      expect(response).to eq("$5\r\nHello\r\n")
+      expect(client).to receive(:puts).with("$5\r\nHello\r\n")
+      server.execute(client,%w[ECHO Hello])
     end
 
     it "returns an error when no argument is provided for ECHO" do
-      response = send_command("*1\r\n$4\r\nECHO\r\n")
-      expect(response).to eq("-ERR wrong number of arguments for 'echo' command\r\n")
+      expect(client).to receive(:puts).with("-ERR wrong number of arguments for 'echo' command\r\n")
+      server.execute(client,%w[ECHO])
+
     end
 
     it "sets a value to the given key using SET command" do
-      response = send_command("*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n")
-      expect(response).to eq("+OK\r\n")
+      expect(client).to receive(:puts).with("+OK\r\n")
+      server.execute(client,%w[SET foo bar])
     end
 
     it "gets the value to the given key using GET command" do
-      send_command("*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n")
-      response = send_command("*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n")
-      expect(response).to eq("$5\r\nvalue\r\n")
+      server.store["key"] = "value"
+      expect(client).to receive(:puts).with("$5\r\nvalue\r\n")
+      server.execute(client,%w[GET key])
     end
 
     it "returns (nil) when given key is not present in redis" do
-      response = send_command("*2\r\n$3\r\nGET\r\n$3\r\nbar\r\n")
-      expect(response).to eq("$-1\r\n")
+      expect(client).to receive(:puts).with("$-1\r\n")
+      server.execute(client, %w[GET non_existing_key])
     end
   end
 end
